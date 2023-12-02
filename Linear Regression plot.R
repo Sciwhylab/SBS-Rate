73f162f5a3d8fe655fc0cb484cf352d3b1c4a4cf
akash-ajay <31865925+akash-ajay@users.noreply.github.com>
2023-12-03 02:41:02 +0530

    Update Linear Regression plot.R

--- Linear Regression plot.R
+++ Linear Regression plot.R
@@ -16,11 +16,11 @@ library(dplyr)
 ggplot(data = imputed, aes(x = GC, y = log_rt)) + 
   geom_point(color='blue') +
   geom_smooth(method = "lm")+
-  ggtitle(label = "Genomic GC vs Log Substitution rate", subtitle = "Linear regression")+
+  ggtitle(label = "Genomic GC vs Log SNM rate", subtitle = "Linear regression")+
   theme(plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5))+
   xlab(label = "Genomic GC%")+
-  ylab(label = "log substitution rate(bp/gen)")+
+  ylab(label = "log SNM rate(bp/gen)")+
   scale_x_continuous(breaks = scales::pretty_breaks(n = 8))+
   scale_y_continuous(breaks = scales::pretty_breaks(n = 8))
 
